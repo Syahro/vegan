b91db732f92e70a34a688a353bf9cd1bcf6a5ea4
Arief Syahroni <riefsyah31@gmail.com>
2021-08-23 12:01:21 +0700

    done modified noConnectionPage and add some assets or support

---
+++ lib/pages/no_connection_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:vegan/theme.dart';
+
+class NoConnectionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin + 4),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/illustration.png',
+                width: 319,
+                height: 229,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Text(
+                'Oops! Madam?',
+                style: semiBoldTextStyle.copyWith(
+                  fontSize: 22,
+                  color: semiBlackColor,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Tampaknya Anda tidak terhubung\ndengan koneksi Internet',
+                style: regularTextStyle.copyWith(
+                  fontSize: 16,
+                  color: lightGreyColor,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                width: 210,
+                height: 45,
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Try Again',
+                    style: semiBoldTextStyle.copyWith(
+                      fontSize: 18,
+                      color: semiBlackColor,
+                    ),
+                  ),
+                  color: yellowColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(57),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final double defaultMargin = 24.0;
+
+Color backgroundColor = Color(0XFFF5F3F0);
+Color yellowColor = Color(0XFFFEBD2E);
+Color darkGreyColor = Color(0XFF9E9D9B);
+Color lightGreyColor = Color(0XFF828282);
+Color orangeColor = Color(0XFFFB6C55);
+Color semiBlackColor = Color(0XFF242424);
+
+TextStyle semiBoldTextStyle = GoogleFonts.nunito(
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle mediumTextStyle = GoogleFonts.nunito(
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle regularTextStyle = GoogleFonts.nunito(
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle lightTextStyle = GoogleFonts.nunito(
+  fontWeight: FontWeight.w300,
+);
